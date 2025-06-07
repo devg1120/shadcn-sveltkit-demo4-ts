404476107c4149b0402ee8523af9e9cf4fd71ecd
devg1120 <devg1120@gmail.com>
2025-06-07 17:52:38 +0900

    up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,3 +9,16 @@
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<hr/>
+
+<ul>
+ <li><a href="/mail">mail</a></li>
+ <li><a href="/dashboard">dashboard</a></li>
+ <li><a href="/auth">auth</a></li>
+ <li><a href="/cards">cards</a></li>
+ <li><a href="/colors">colors</a></li>
+ <li><a href="/music">music</a></li>
+ <li><a href="/tasks">tasks</a></li>
+</ul>
+
